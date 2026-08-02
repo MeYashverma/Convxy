90b844682a7a7515a2e6354c83489e53d91213db
CosmicIsAryan <146110938+CosmicIsAryan@users.noreply.github.com>
2026-08-02 13:59:22 +0530

    chore: bump version to 1.4, update changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.convx.music"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.3"
+        versionCode = 3
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
